4d587df4450f15b922df61760b8c78a23e0510fd
aristachauhan <aristachauhan@gmail.com>
2026-07-08 16:18:39 +0000

    cluster and department db tables

---
+++ server.core/Domain/Department.cs
@@ -0,0 +1,53 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Server.Core.Domain;
+
+[Table("Department")]
+public class Department
+{
+    [Key]
+    [Column(TypeName = "nvarchar(10)")]
+    [MaxLength(10)]
+    public required string DepartmentCode { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string DepartmentName { get; set; }
+
+    public byte? SourceLevel { get; set; }
+
+    public int? ClusterId { get; set; }
+
+    public Cluster? Cluster { get; set; }
+
+    [Required]
+    public WorkflowMode WorkflowMode { get; set; } = WorkflowMode.DirectSubmission;
+
+    [Required]
+    public bool IsActive { get; set; } = true;
+
+    public DateTime? LastSeenInSourceAt { get; set; }
+
+    [Required]
+    [Column(TypeName = "datetime2")]
+    public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    [Column(TypeName = "datetime2")]
+    public DateTime UpdatedUtc { get; set; } = DateTime.UtcNow;
+
+    public static void Configure(EntityTypeBuilder<Department> entity)
+    {
+        entity.HasKey(e => e.DepartmentCode);
+        entity.Property(e => e.IsActive).HasDefaultValue(true);
+        entity.Property(e => e.WorkflowMode).HasDefaultValue(WorkflowMode.DirectSubmission);
+
+        entity.HasOne(e => e.Cluster)
+            .WithMany(e => e.Departments)
+            .HasForeignKey(e => e.ClusterId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
---
+++ server.core/Domain/WorkflowMode.cs
@@ -0,0 +1,7 @@
+namespace Server.Core.Domain;
+
+public enum WorkflowMode
+{
+    DirectSubmission = 0,
+    ApprovalRequired = 1,
+}
